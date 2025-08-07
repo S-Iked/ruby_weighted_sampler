f9beb136d8d9ff3c899d6a0c9e4b405dc45b4e83
shinji.ikeda <shinji.ikeda@supership.jp>
2025-08-07 21:54:49 +0900

    update

--- spec/weighted_sampler_spec.rb
+++ spec/weighted_sampler_spec.rb
@@ -6,6 +6,19 @@ RSpec.describe WeightedSampler do
   end
 
   it "test case 1" do
+    ws = WeightedSampler.new({"a": 9, "b": 1})
+    ret = {"a": 0, "b": 0}
+    100.times.each do |n|
+      _r = ws.get
+      ret[_r] += 1
+    end
+    expect(ret[:a] > ret[:b]).to eq true
+    ret.each do |k, v|
+      expect(v).to be >= 1
+    end
+  end
+
+  it "test case 2" do
     ws = WeightedSampler.new({"a": 1, "b": 9})
     ret = {"a": 0, "b": 0}
     100.times.each do |n|
@@ -13,15 +26,21 @@ RSpec.describe WeightedSampler do
       ret[_r] += 1
     end
     expect(ret[:a] < ret[:b]).to eq true
+    ret.each do |k, v|
+      expect(v).to be >= 1
+    end
   end
 
-  it "test case 2" do
-    ws = WeightedSampler.new({"a": 1, "b": 9, "c": 5})
+  it "test case 3" do
+    ws = WeightedSampler.new({"a": 1, "b": 9, "c": 3})
     ret = {"a": 0, "b": 0, "c": 0}
     100.times.each do |n|
       _r = ws.get
       ret[_r] += 1
     end
     expect(ret[:a] < ret[:b] && ret[:c] < ret[:b]).to eq true
+    ret.each do |k, v|
+      expect(v).to be >= 1
+    end
   end
 end
